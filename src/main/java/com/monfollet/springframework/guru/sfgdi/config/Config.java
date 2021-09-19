5d3421d7bfb59a84039e90cf5581a4a8733f6f60
romain <monfollet.romain@hotmail.fr>
2021-09-19 22:16:09 +0200

    overriding properties with env vars and command line arguments

--- src/main/java/com/monfollet/springframework/guru/sfgdi/config/Config.java
+++ src/main/java/com/monfollet/springframework/guru/sfgdi/config/Config.java
@@ -1,13 +1,30 @@
 package com.monfollet.springframework.guru.sfgdi.config;
 
+import com.monfollet.springframework.guru.sfgdi.datasource.FakeDataSource;
 import com.monfollet.springframework.guru.sfgdi.domain.Counter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
+
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class Config {
+
+    @Bean
+    FakeDataSource fakeDataSource(
+            @Value("${guru.username}") final String username,
+            @Value("${guru.password}") final String password,
+            @Value("${guru.jdbcurl}") final String jdbcUrl) {
+        final FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcUrl(jdbcUrl);
+        return fakeDataSource;
+    }
 
     @Bean
     public Counter counter() {
