3ceeff232548a4819c5648156ec6101ca248e7f3
romain <monfollet.romain@hotmail.fr>
2021-09-19 22:25:03 +0200

    using spring boot's application.properties and discarding @PropertySource use

--- src/main/java/com/monfollet/springframework/guru/sfgdi/config/Config.java
+++ src/main/java/com/monfollet/springframework/guru/sfgdi/config/Config.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class Config {
